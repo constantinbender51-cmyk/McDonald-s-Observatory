c9b7a8b6d72e95c8c498cd0c428bdc5382491353
constantinbender51-cmyk <constantinbender51@gmail.com>
2025-10-09 16:10:21 +0200

    Update macd_single.py

--- macd_single.py
+++ macd_single.py
@@ -62,12 +62,13 @@ for i in range(1, len(df)):
         entry_d      = df['date'].iloc[i]
         just_entered = True
         stp          = False
-        if (1 - p_now/p_prev) * in_pos >= stp_pct:
+        r_hi = (p_prev / df['high'].iloc[i] - 1) * in_pos
+        r_lo = (p_prev / df['low'].iloc[i]  - 1) * in_pos
+        if  r_hi >= stp_pct or r_lo >= stp_pct:
             stp = True
             stp_price = curve[-1] * (1 - stp_pct * LEVERAGE)
             stp_cnt += 1
             stp_cnt_max = max(stp_cnt_max, stp_cnt)
-            curve.append(stp_price)         # no P&L on entry day
         else: 
             curve.append(curve[-1] * (1 + (p_now/p_prev - 1) * in_pos * LEVERAGE))         # no P&L on entry day
         
